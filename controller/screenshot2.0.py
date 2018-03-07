0634b63c438583f7cbacb6cb9fe1dd86bb3da1ac
Amit Vardi <amitvardi1@gmail.com>
2018-03-07 11:00:20 +0200

    add ip global variable

--- controller/screenshot2.0.py
+++ controller/screenshot2.0.py
@@ -4,10 +4,11 @@ __author__ = 'Vardi'
 import cv2
 import socket
 END_DATA_MESSAGE = "No More Data"
+IP = "192.168.30.31"
 def main():
     print "Client Searching"
     client_socket = socket.socket()
-    client_socket.connect(("192.168.30.31",2345))  # Client Startup
+    client_socket.connect((IP,2346))  # Client Startup
     print "Server Found"
     cv2.namedWindow("image",cv2.WND_PROP_FULLSCREEN)  # Put Window to Fullscreen mode
     cv2.setWindowProperty("image",cv2.WND_PROP_FULLSCREEN,cv2.WINDOW_FULLSCREEN)  # Set Properties of window to fullscreen
@@ -23,6 +24,7 @@ def main():
             cv2.imshow("image" ,img)  # Show the image onscreen
             cv2.waitKey(1)  # Wait for Key 1 second, if not continue
         except:
+            exit()
             continue
 
 
