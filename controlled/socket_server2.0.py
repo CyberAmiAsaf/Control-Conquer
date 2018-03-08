aca178ee8bfaad5cba2fbf9232b532e856a6b93a
Amit Vardi <amitvardi1@gmail.com>
2018-03-08 09:44:29 +0200

    Added global variables for port and delay and file name

--- controlled/socket_server2.0.py
+++ controlled/socket_server2.0.py
@@ -2,11 +2,13 @@ __author__ = 'Vardi'
 import socket
 import time
 from PIL import ImageGrab
-
+SCREEN_PORT = 2346
+DELAY_TIME = 0.05
+SCREENSHOT_NAME = "scrn.png"
 def main():
     print "Server Running"
     server_socket = socket.socket()
-    server_socket.bind(('0.0.0.0',2346))  # Start the socket's server
+    server_socket.bind(('0.0.0.0',SCREEN_PORT))  # Start the socket's server
     server_socket.listen(5)
     (new_socket, address) = server_socket.accept()
     print "Client Connected"
@@ -14,14 +16,14 @@ def main():
     while True:
         last_binary_data = ""
         img = ImageGrab.grab(bbox=None)  # Take screenshot
-        img.save("scrn.png")  # Save screenshot
-        fp = open("scrn.png",'rb')  # open screenshot file to read
+        img.save(SCREENSHOT_NAME)  # Save screenshot
+        fp = open(SCREENSHOT_NAME,'rb')  # open screenshot file to read
         data = fp.read()  # Read all
         while data != last_binary_data:  # while not all the data is sent
             new_socket.send(data)  # Send all data
             last_binary_data = data
         fp.close() # Close file
-        time.sleep(0.05)  # Delay in order to boost quality
+        time.sleep(DELAY_TIME)  # Delay in order to boost quality
 
 
 if __name__ == '__main__':
